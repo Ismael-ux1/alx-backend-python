b647d5fd2ef64beae4c264730f211e9269b62cb7
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-01-12 22:04:41 +0000

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,25 +3,27 @@
 from typing import Tuple, List, Any
 
 
-# Function to zoom an array
+""" Function to zoom an array """
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    # Create a new list
-    # where each item in the original list is repeated 'factor' times
+    """
+    Create a new list
+    where each item in the original list is repeated 'factor' times
+    """
     zoomed_in = [
-            # For each item in the original list
+            """ For each item in the original list """
             item for item in lst
-            # Repeat 'factor' times
+            """ Repeat 'factor' times """
             for i in range(factor)
     ]
-    # Return the new zoomed in list
+    """ Return the new zoomed in list """
     return zoomed_in
 
 
-# Define an array as a tuple
+""" Define an array as a tuple """
 array = (12, 72, 91)
 
-# Call the zoom_array function with the array and default factor of 2
+""" Call the zoom_array function with the array and default factor of 2 """
 zoom_2x = zoom_array(array)
 
-# Call the zoom_array function with the array and a factor of 3
+""" Call the zoom_array function with the array and a factor of 3 """
 zoom_3x = zoom_array(array, 3)
