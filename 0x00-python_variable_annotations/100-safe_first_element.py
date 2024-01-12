9803fa3f837992a6c7aa95aaa493430144a9e7db
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-01-12 18:28:12 +0000

    Duck typing - first element of a sequence

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,34 +1,22 @@
-#!/usr/bin/env python3
-
-""" 
-This function returns the value of a key in a dictionary if it exists,
-otherwise it returns a default value. 
+#!/usr/bin/python3
 """
+A function that Returns the first element of the list if it exists,
+other wise returns None
+"""
+from typing import Optional, Any, Sequence
 
-from typing import TypeVar, Dict, Optional
 
-
-# Define TypeVars for key and value types
-KT = TypeVar('KT')
-VT = TypeVar('VT')
-
-def safely_get_value(dct: Dict[KT, VT], key: KT, default: Optional[VT] = None) -> Optional[VT]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """
-    This function returns the value of a key in a dictionary if it exists,
-    otherwise it returns a default value.
-
     Parameters:
-    dct (Dict[KT, VT]): The dictionary to search.
-    key (KT): The key to look for in the dictionary.
-    default (Optional[VT]): The default value to return if the key is not found.
+    - lst (Sequence[Any]): Input sequence.
 
     Returns:
-    Optional[VT]: The value of the key in the dictionary or the default value.
+    - Optional[Any]: The first element of the sequence or,
+      None if the sequence is empty.
     """
-    # Check if the key is in the dictionary
-    if key in dct:
-        # If the key is found, return its value
-        return dct[key]
+
+    if lst:
+        return lst[0]
     else:
-        # If the key is not found, return the default value
-        return default
+        return None
